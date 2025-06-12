e138f280b20cfa05b28837d705bb9bba00a58b5e
Nihil <diisisiwhjif@gmail.com>
2025-06-12 15:29:42 +0500

    Save

---
+++ Assets/_Project2D/_Scripts/SeedManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class SeedManager : MonoBehaviour
+{
+
+    #region FIELDS
+
+        [Header("NOTES")] [TextArea(4, 10)]
+        public string notes;
+
+        [Header("VARIABLES")]
+            
+            [Header("Basic Variables")]
+            public int seed = 1;
+            public bool useRandomSeed = false;
+
+    #endregion
+
+    #region LIFE CYCLE METHODS
+
+        /// <summary>
+        /// Called when the script instance is being loaded.
+        /// Useful for initialization before the game starts.
+        /// </summary>
+        private void Awake()
+        {
+            InitializeSeed();
+        }
+
+        /// <summary>
+        /// Called before the first frame update.
+        /// Useful for initialization once the game starts.
+        /// </summary>
+        private void Start()
+        {
+            // Perform initial setup that occurs when the game starts.
+            // Example: Initialize game state, start coroutines, load resources, etc.
+        }
+
+        /// <summary>
+        /// Called once per frame.
+        /// Use for logic that needs to run every frame, such as user input or animations.
+        /// </summary>
+        private void Update()
+        {
+            // Add your per-frame logic here.
+            // Example: Move objects, check user input, update animations, etc.
+        }
+
+        /// <summary>
+        /// Called at fixed intervals, ideal for physics updates.
+        /// Use this for physics-related updates like applying forces or handling Rigidbody physics.
+        /// </summary>
+        private void FixedUpdate()
+        {
+            // Add physics-related logic here.
+            // Example: Rigidbody movement, applying forces, or collision detection.
+        }
+
+    #endregion
+
+    #region CUSTOM METHODS
+
+        /// <summary>
+        /// Setting up a seed.
+        /// </summary>
+        private void InitializeSeed()
+        {
+            if (useRandomSeed) seed = System.Guid.NewGuid().GetHashCode();
+            
+            UnityEngine.Random.InitState(seed);
+        }
+
+    #endregion
+
+}
